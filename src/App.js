a2da68db13b47057360f6cd71fec47b0dab97d6d
peiqi <peiqi@qq.com>
2021-06-11 04:39:17 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React,{Component} from 'react'
+import {Button, message} from 'antd'
+import { BrowserRouter,HashRouter,Switch,Route } from 'react-router-dom'
 
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 export default class App extends Component {
-    render(){
-        return (<div>App</div>)
+    handleClick = () => {
+        message.success('成功啦...');
     }
+    
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/" component={Admin}/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+    
 }
